9d61d6028540887b04c86ff3dee782252a8a5047
Krish-Choudhary123 <93879650+Krish-Choudhary123@users.noreply.github.com>
2022-08-10 09:56:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+console.log("134.765.7392");
+console.log("Sub To Valorus cuz im better than tanqr trust");
+
+console.log("Sub To Valorus cuz im better than tanqr trust");
+
+
+console.log("Sub To Valorus cuz im better than tanqr trust");
+
+
+console.log("Sub To Valorus cuz im better than tanqr trust");
+
+
+
+console.log("Sub To Valorus cuz im better than tanqr trust");
+
+
